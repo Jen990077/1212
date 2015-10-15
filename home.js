522991158271cee1b836e5a2f3c6d6bf680cfa68
Jen990077 <jenleverenz@yahoo.com.sg>
2015-10-15 19:39:34 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,21 +1,16 @@
-
 var Home = {}
+
+Home.ButtonClick = function () {
+    $.ajax
+     ({
+         url: "Home/GetPlayerInformation",
+         data: { PlayerNumber: $(".output").val() },
+         success: function (result) {append(result); }
+     });
+}
+
 $(document).ready(function () {
     $(".button").click(Home.ButtonClick);
 
 });
 
-
-
-
-
-Home.ButtonClick = function () {
-    $.ajax
-    ({
-        url: "Home/GetPlayerInformation",
-        type: "POST",
-        dataType: "html",
-        data: content,
-        success: function (data) { $(".output").append(data); }
-    });
-}
